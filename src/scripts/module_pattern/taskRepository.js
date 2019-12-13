7fea552a6b886bcd321d0e28dc0231c7b6f7c2f5
vishwajeethitk <vishawjeethitk@gmail.com>
2019-12-13 14:29:17 +0530

    Modified the return object from TaskRepository

--- src/scripts/module_pattern/taskRepository.js
+++ src/scripts/module_pattern/taskRepository.js
@@ -1,16 +1,22 @@
 var repo = function() {
-    return {
-        get: function(id) {
-            console.log("Getting task " + id);
-            return {
-                name: "new task from db"
-            }
-        },
-        
-        save: function(task) {
-            console.log("Saving " + task.name + " to db");
+
+    var db = {/* Connection strings here */};
+
+    var get = function(id) {
+        console.log("Getting task " + id);
+        return {
+            name: "new task from db"
         }
     }
+    
+    var save = function(task) {
+        console.log("Saving " + task.name + " to db");
+    }
+
+    return {
+        get: get,
+        save: save
+    } 
 }
 
 module.exports = repo();
