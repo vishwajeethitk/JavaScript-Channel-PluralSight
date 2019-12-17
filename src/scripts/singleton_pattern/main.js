2110ec4428408f7be772d655b5f9da84c581e177
vishwajeethitk <vishawjeethitk@gmail.com>
2019-12-17 16:29:06 +0530

    Added examples of singleton pattern

---
+++ src/scripts/singleton_pattern/main.js
@@ -0,0 +1,13 @@
+var taskHandler = require('./taskHandler');
+//var repo = require('./repo');
+var myrepo = require('./repo');
+//var myrepo = repo();
+
+myrepo.save('from main')
+myrepo.save('from main')
+myrepo.save('from main')
+
+taskHandler.save()
+taskHandler.save()
+taskHandler.save()
+taskHandler.save()
