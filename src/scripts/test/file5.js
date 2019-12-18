73b3a9d056231001c82b9342e093a383fbbd7a9c
vishwajeethitk <vishawjeethitk@gmail.com>
2019-12-18 18:32:37 +0530

    Irrelevant commit

---
+++ src/scripts/test/file5.js
@@ -0,0 +1,58 @@
+// for(var i=0; i<10; i++) {
+//     function timer(j) {
+//         setTimeout(function() {
+//             console.log(j);
+
+//         }, j*1000);
+//     };
+
+//     timer(i);
+// }
+
+// [...input].forEach(c=>{
+//     arr[c.charCodeAt(c)-97]++;
+// });
+//   for(let i in arr){
+//     if(arr[i]==0){
+//       isPangramResp = false;
+//       return isPangramResp;
+//     }
+//   }
+
+// function makeFunc() {
+//     {
+//         let name = 'hello';
+//     }
+//     // let name = 'hello';
+    
+//     function displayName() {
+//         console.log(name);
+//     }
+
+//     function changeName(change) {
+//         name = change;
+//     }
+
+//     name = 'hello1';
+//     return {
+//         displayName,
+//         changeName
+//     };
+// }
+
+// var myFunc = makeFunc();
+// myFunc.displayName();
+//myFunc.changeName('hello1');
+//myFunc.displayName();
+
+for(let i=0; i<5; i++) {
+    setTimeout(function() {
+        console.log(i);
+    }, 0);
+
+    //i++;
+}
+// {
+//     let i = 0;
+// }
+// console.log(i)
