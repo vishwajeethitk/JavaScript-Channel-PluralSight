1838c6abc8d849f9adf938795e96752f7bae7682
vishwajeethitk <vishawjeethitk@gmail.com>
2019-12-18 18:55:02 +0530

    Added decorator pattern example

--- src/scripts/design_patterns/structural_design_patterns/decorator_pattern.js
+++ src/scripts/design_patterns/structural_design_patterns/decorator_pattern.js
@@ -16,3 +16,25 @@ var myTask = new Task('Legacy Task');
 myTask.complete();
 myTask.save();
 
+/* Decorator pattern implemented below */
+var UrgentTask = function(name, priority) {
+    Task.call(this, name);
+    this.priority = priority;
+}
+
+UrgentTask.prototype = Object.create(Task.prototype);
+
+UrgentTask.prototype.notify = function() {
+    console.log('notifying important people');
+}
+
+UrgentTask.prototype.save = function() {
+    console.log('do special stuff before saving');
+    Task.prototype.save.call(this);
+}
+
+var urgentTask = new UrgentTask('This is urgent', 1);
+
+urgentTask.complete();
+urgentTask.save();
+console.log(urgentTask);
